82d8704ecc724f684e2ff380b414b5228560f3f3
PersesTitan <torampersesTitan@gmail.com>
2022-11-26 11:02:55 +0900

    add start

--- create.js
+++ create.js
@@ -1,9 +1,23 @@
 count = 0;
 score = 0;
-hp = 100;
+hp = 10;
+items = []
+
+function start() {
+    items.forEach(v => clearInterval(v))
+    count = 0;
+    score = 0;
+    hp = 10;
+    items = []
+    document.getElementById('score').innerText = score;
+    document.getElementById('hp').innerText = hp;
+    document.getElementById('btn').style.visibility = "false";
+    document.getElementById('main').innerText = "";
+}
 
 function loop() {
-    const moveInterval = setInterval(() => {
+    start();
+    const createInterval = setInterval(() => {
         const main = document.getElementById("main");
 
         const div = document.createElement("div");
@@ -23,6 +37,7 @@ function loop() {
         // 아이디 추가
         div.setAttribute('id_value', (count++).toString());
 
+        let speedValue = speed(count + 10)/100;
         const moveInterval = setInterval(() => {
             const value = div.style.top;
             const pos = value.substring(0, value.length - 2);
@@ -30,20 +45,23 @@ function loop() {
                 downHp();
                 remove(div.getAttribute('id_value'), moveInterval);
             }
-            div.style.top = pos - 1 + "px"
-        }, speed());
+            div.style.top = pos - speedValue + "px";
+        }, 1);
+        items.push(moveInterval)
 
         // 클릭하면 점수올라가고 제거
-        div.onclick = function () {
+        div.onclick = function() {
             upScore();
             remove(div, moveInterval);
+            items.remove(speedValue);
         }
         main.appendChild(div);
-    });
-}
 
-function clear() {
-    document.getElementById("main").innerHTML = '';
+        if (hp === 0) {
+            remove(div, moveInterval);
+            alarm(createInterval);
+        }
+    }, 1000)
 }
 
 function position(width) {
@@ -53,4 +71,12 @@ function position(width) {
 // min ~ max
 function random(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function alarm(moveInterval) {
+    // 반복 멈추기
+    clearInterval(moveInterval);
+    alert(score + "점");
+    start();
+    document.getElementById('btn').style.visibility = "true";
 }
